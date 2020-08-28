1a77b79978a4a2bae3b684ad707e3235d8b9d9c5
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-08-28 20:21:08 +0530

    View component props are studied

--- App.tsx
+++ App.tsx
@@ -1,19 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import ClipboardAccess from './app/ClipboardAccess';
+import { StyleSheet, View } from 'react-native';
 import { ViewComponent } from './app/examples';
-import ExpoConstants from './app/ExpoConstants';
 
 export default function App() {
-	console.log('process', process);
 	return (
 		<View style={styles.container}>
 			<ViewComponent />
-			<ClipboardAccess />
-			<ExpoConstants />
-			<Text>Build type: {process.env.REACT_APP_BUILD}</Text>
-			<StatusBar style="auto" />
 		</View>
 	);
 }
--- app/examples/ViewComponent.tsx
+++ app/examples/ViewComponent.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function ViewComponent() {
+	/**
+	 * accessible - the flag set to true will enable the view or component to be an
+	 * accessibility element that can be read by VoiceOver for people with disabilities.
+	 */
+	/**
+	 * collapsable - Views that are only used to layout their children or otherwise don't draw
+	 * anything may be automatically removed from the native hierarchy as an optimization.
+	 * Set this property to false to disable this optimization and
+	 * ensure that this View exists in the native view hierarchy.
+	 */
 	return (
-		<View>
+		<View accessible={true} collapsable>
 			<Text>View</Text>
 		</View>
 	);
