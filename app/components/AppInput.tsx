3635c47f06eaf6f25e0d8dc9f3495adabc47373f
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-08-29 12:19:53 +0530

    input filed props tested

--- app/components/AppInput.tsx
+++ app/components/AppInput.tsx
@@ -5,12 +5,22 @@ const AppInput = () => {
 	const [value, onChangeText] = useState('Useless Placeholder');
 
 	return (
-		<View>
-			<Text>AppInput</Text>
+		<View style={styles.container}>
+			<Text>AppInput BOs 12w</Text>
 			<TextInput
-				style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+				style={{
+					height: 40,
+					borderColor: 'gray',
+					borderWidth: 1,
+					width: '100%',
+					backgroundColor: '#ccc',
+				}}
 				onChangeText={(text) => onChangeText(text)}
 				value={value}
+				defaultValue={'what is the new content'}
+				autoCapitalize={'words'}
+				keyboardAppearance="dark"
+				keyboardType="number-pad"
 			/>
 		</View>
 	);
@@ -18,4 +28,10 @@ const AppInput = () => {
 
 export default AppInput;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+});
