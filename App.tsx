ee75b6565760eeb6feb227e5ac1407f1a1824ce0
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-08-30 18:48:15 +0530

    refreshcontrol component example added

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import PressableExample from './app/examples/PressableExample';
+import RefreshControlExample from './app/examples/RefreshControlExample';
 
 export default function App() {
-	return <View style={styles.container}>{<PressableExample />}</View>;
+	return <View style={styles.container}>{<RefreshControlExample />}</View>;
 }
 
 const styles = StyleSheet.create({
---
+++ app/examples/RefreshControlExample.tsx
@@ -0,0 +1,56 @@
+import Constants from 'expo-constants';
+import React from 'react';
+import {
+	RefreshControl,
+	SafeAreaView,
+	ScrollView,
+	StyleSheet,
+	Text,
+} from 'react-native';
+
+const wait = (timeout) => {
+	return new Promise((resolve) => {
+		setTimeout(resolve, timeout);
+	});
+};
+
+const RefreshControlExample = () => {
+	const [refreshing, setRefreshing] = React.useState(false);
+
+	const onRefresh = React.useCallback(() => {
+		setRefreshing(true);
+
+		wait(2000).then(() => setRefreshing(false));
+	}, []);
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<ScrollView
+				contentContainerStyle={styles.scrollView}
+				refreshControl={
+					<RefreshControl
+						refreshing={refreshing}
+						onRefresh={onRefresh}
+					/>
+				}
+			>
+				<Text>Pull down to see RefreshControl indicator</Text>
+			</ScrollView>
+		</SafeAreaView>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		marginTop: Constants.statusBarHeight,
+	},
+	scrollView: {
+		flex: 1,
+		backgroundColor: 'pink',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
+
+export default RefreshControlExample;
