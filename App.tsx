7dd8c50cb4697bdda54481f90c0d7e83485aedb7
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-09-01 21:43:36 +0530

    AppText sample Text component testing process started

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import StatusBarExample from './app/examples/StatusBarExample';
+import AppText from './app/components/AppText';
 
 export default function App() {
-	return <View style={styles.container}>{<StatusBarExample />}</View>;
+	return <View style={styles.container}>{<AppText />}</View>;
 }
 
 const styles = StyleSheet.create({
---
+++ app/components/AppText.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const AppText = () => {
+	return (
+		<View>
+			<Text>AppText</Text>
+		</View>
+	);
+};
+
+export default AppText;
+
+const styles = StyleSheet.create({});
