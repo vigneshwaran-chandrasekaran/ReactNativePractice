31f04f20b891f80e4d73304a4a528535874985f2
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-09-04 23:06:16 +0530

    using Animated, moving the object right, left done

--- app/RNApi/AnimatedBallExample.tsx
+++ app/RNApi/AnimatedBallExample.tsx
@@ -13,17 +13,44 @@ const AnimatedBallExample = () => {
      * 1. useRef - will return same object
      * 2. useState
      */
-    const value = useState(new Animated.Value(2))[0];
+
+     /**
+      * in general js, if we move some object means we use position property,
+      * but if we do it like in RN, it will so much computation,
+      * to avoid that use RN Animated
+      */
+    const leftValue = useState(new Animated.Value(-50))[0];
     const fadeAnim = useRef(new Animated.Value(4)).current;
 
-    console.log('value', value);
+    console.log('value', leftValue);
     console.log('fadeAnim', fadeAnim);
 
+    function moveBall() {
+        console.log('moveBall', fadeAnim);
+        Animated.timing(leftValue, {
+            toValue: 100,
+            duration: 1000,
+            useNativeDriver:false
+        }).start()
+    }
+
+    function goLeft() {
+        console.log('goLeft', leftValue);
+        Animated.timing(leftValue, {
+            toValue: 0,
+            duration: 1000,
+            useNativeDriver:false
+        }).start()
+    }
+
     return (
-        <View>
-            <View style={styles.ball} /> 
-            <TouchableOpacity>
-            <Text>Click me</Text> 
+        <View style={styles.container}>
+            <Animated.View style={[styles.ball, {marginLeft:leftValue}]} /> 
+            <TouchableOpacity onPress={moveBall}>
+                <Text>Go right</Text> 
+            </TouchableOpacity>
+            <TouchableOpacity onPress={goLeft}>
+                <Text>Go left</Text> 
             </TouchableOpacity>
         </View>
     )
@@ -32,6 +59,11 @@ const AnimatedBallExample = () => {
 export default AnimatedBallExample
 
 const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        alignItems: 'center',
+		justifyContent: 'center',
+    },
     ball: {
         width: 100,
         height:100,
