1974d4b6cb4ac3565c64322590fcf7471880fcc5
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-09-09 22:51:18 +0530

    using navigation prop we can move between different routes added

--- app/screens/DetailsScreen.tsx
+++ app/screens/DetailsScreen.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-const DetailsScreen = () => {
+const DetailsScreen = ({ navigation }) => {
 	return (
 		<View
 			style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
 		>
 			<Text>Details Screen 222</Text>
+			<Button
+				title="Go to Home"
+				onPress={() => navigation.navigate('Home')}
+			/>
+			{/* we already in same page so nothing happened */}
+			<Button
+				title="Go to Details... again"
+				onPress={() => navigation.navigate('Details')}
+			/>
 		</View>
 	);
 };
--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
 	return (
 		<View
 			style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
 		>
 			<Text>Home Screen abc</Text>
+			<Button
+				title="Go to Details"
+				onPress={() => navigation.navigate('Details')}
+			/>
+			{/* By using push we once again add, same route in stack queue */}
+			<Button
+				title="Go to Details... again"
+				onPress={() => navigation.push('Details')}
+			/>
 		</View>
 	);
 };
